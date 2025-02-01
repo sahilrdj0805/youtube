920101445aea3768c0e95807f458a8a9f7b50a92
Sahil Ahamad <sahilahmadnke@gmail.com>
2025-02-01 22:21:35 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,4 +1,4 @@
-const apiKey = "AIzaSyCooBA52qMDmlPs15fs2-epsI3MhCB4luI";
+const apiKey = "AIzaSyB_hbfD4Z6lC0neHeRqyeUM-D_NSc2sRqA";
 const urlParams = new URLSearchParams(window.location.search);
 const query = urlParams.get("q");
 
